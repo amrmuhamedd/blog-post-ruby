69219f6be629954d5024290ef3f808ccde46553c
Amr Mohamed <amrmuhmedd@gmail.com>
2022-11-30 12:37:31 +0200

    add getbyid and create method to blog

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,9 +1,23 @@
 module Api
    module V1
         class BlogsController < ApplicationController
+            ALLOWED_DATA = %[title description content user].freeze
             def index
                 blogs = Blog.all
                 render json: blogs
+            end
+            def show
+                blog = Blog.find(params[:id])
+                render json: blog
+            end
+            def create
+                data = json_payload.select {|k| ALLOWED_DATA.include?(k)}
+                blog = Blog.new(data)
+                if blog.save
+                    render json: blog
+                else
+                    render json: {"error": "couldnt creat it"}
+                end
             end
         end
    end
